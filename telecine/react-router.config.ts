8c9a32d278e917ca57549aa62a83fa8eb8ebca92
Collin Miller <collintmiller@gmail.com>
2026-02-19 00:44:40 -0600

    fix react-router config resolution in production Docker build
    react-router build --config derives rootDirectory from the config
    file path, not CWD. This caused react-router.config.ts (at /app/) to
    not be found when rootDirectory resolved to /app/services/web/,
    leaving v8_middleware as false and disabling middleware in production.

--- telecine/react-router.config.ts
+++ telecine/react-router.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: true,
   appDirectory: "./services/web/app",
+  buildDirectory: "./services/web/build",
   future: {
     v8_middleware: true,
   },
